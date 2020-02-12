9dde78a9c140663574e3b8474943e865143ea76d
ferran <ferranabaarb@monlau.com>
2020-02-12 15:56:22 +0100

    Version 1.0

---
+++ PracticaDNI/PracticaDNI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaDNI
+{//Ferran
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dni = Convert.ToInt32(textBoxDNI.Text);
+            char letter = functionDNI(dni);
+            textBoxLetter.Text = letter.ToString();
+            MessageBox.Show("Done");
+        }
+        private char functionDNI(int dni)
+        {
+            char letras[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+            int posicion = dni % 23;
+
+            return letras[posicion];
+        }
+    }
+}
